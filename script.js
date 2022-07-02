8b3f7db7e51bb2f80c3c7d9b8499e68f784cb32c
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-02 17:15:52 +0800

    Create random logic for rock, paper, or scissors

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 function computerPlay() {
   // Generate a random number between 1 to 3
   let random = Math.floor(Math.random()*3) + 1;
-    
-  return random;
+
+  // Returns the result 1 = Rock, 2 = Paper, 3 = Scissors 
+  switch (random) {
+    case 1:
+      return 'Rock';
+    case 2: 
+      return 'Paper';
+    case 3:
+      return 'Scissors';
+  }
 }
 
 console.log(computerPlay());
