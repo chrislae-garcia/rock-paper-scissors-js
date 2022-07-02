86bb828743e1c4b34988d78802fd76efe11a41c4
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-02 17:05:11 +0800

    Create computerPlay() function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Test');
+
+function computerPlay() {
+  // Generate a random number between 1 to 3
+  let random = Math.floor(Math.random()*3) + 1;
+    
+  return random;
+}
+
+console.log(computerPlay());
